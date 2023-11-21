1913e5921fcfa7c4a2f6843bc1db238047d8f112
Muhammed Yaseen K C <ymkcmpd@gmail.com>
2023-11-21 16:52:49 +0530

    Added Initial FIles

--- 01-variables-data-types/03-variables/script.js
+++ 01-variables-data-types/03-variables/script.js
@@ -1,6 +1,19 @@
 // Ways to declare a variable
 // var, let, const
 
+const prompt = require("prompt-sync")();
+
+let Name = "Muhammed";
+console.log(Name);
+
+let secondName = "Yaseen K C";
+console.log(secondName);
+
+console.log(Name, secondName);
+
+let age = 10;
+console.log(age);
+
 // Naming Conventions
 // - Only letters, numbers, underscores and dollar signs
 // - Can't start with a number
@@ -10,3 +23,47 @@
 // first_name    underscore
 // FirstName     PascalCase
 // firstname     owercase
+
+let firstName = "a";
+let first_name = "b";
+let FirstName = "c";
+let firstname = "d";
+
+console.log(firstName, first_name, FirstName, firstname);
+
+age = 11;
+
+console.log(age);
+
+let score;
+
+score = 1;
+
+console.log(score);
+
+if (true) {
+  score = score + 1;
+}
+
+console.log(score);
+
+const num = 100;
+
+const arr = [1, 2, 3, 4];
+
+arr.push[5];
+
+console.log(arr);
+
+
+console.log(num);
+
+const person = {
+  name: "yaseen",
+  age: 15,
+};
+
+console.log(person);
+
+person.name = "Hareesh";
+console.log(person);
